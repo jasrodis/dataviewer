22b415d11f36eae19af56473e2adaacb23f5e99c
Iason Rodis <iason-dimitrios.rodis@cern.ch>
2018-04-25 15:35:37 +0200

    Added JavaDoc

--- src/main/java/org/charts/dataviewer/api/data/PlotData.java
+++ src/main/java/org/charts/dataviewer/api/data/PlotData.java
@@ -10,7 +10,7 @@ import com.google.gson.Gson;
 /**
  * A Container of all the Traces to be plotted.
  * 
- * @author irodis
+ * @author jasrodis
  */
 public class PlotData {
 
@@ -20,26 +20,53 @@ public class PlotData {
 	private List<GenericTrace<?>> traces = new ArrayList<>();
 
 	public PlotData() {
+		// Empty
 	}
 
+	/**
+	 * Add all traces into the DataViewer (varargs)
+	 * 
+	 * @param traces
+	 */
 	public PlotData(GenericTrace<?>... traces) {
 		addAll(traces);
 	}
 
+	/**
+	 * Returns a list with all the traces present in the DataViewer.
+	 * 
+	 * @return traces List<GenericTrace<?>>
+	 */
 	public List<GenericTrace<?>> getAllTraces() {
 		return traces;
 	}
 
+	/**
+	 * Adds a single trace to the DataViewer.
+	 * 
+	 * @param trace
+	 *            GenericTrace<?>
+	 */
 	public void addTrace(GenericTrace<?> trace) {
 		traces.add(trace);
 	}
 
+	/**
+	 * Add all traces into the DataViewer (varargs)
+	 * 
+	 * @param traces
+	 */
 	public void addAll(GenericTrace<?>... traces) {
 		for (GenericTrace<?> trace : traces) {
 			this.traces.add(trace);
 		}
 	}
 
+	/**
+	 * Serialize the PlotData object
+	 * 
+	 * @return serialized plotData Object
+	 */
 	public String serialize() {
 		return gson.toJson(this);
 	}

682681867c4701eabe447037eb47388e9ef02471
Iason Rodis <iason-dimitrios.rodis@cern.ch>
2018-04-25 15:36:50 +0200

    Added JavaDoc

--- src/main/java/org/charts/dataviewer/api/data/ResetData.java
+++ src/main/java/org/charts/dataviewer/api/data/ResetData.java
@@ -3,16 +3,21 @@ package org.charts.dataviewer.api.data;
 import com.google.gson.Gson;
 
 /**
- * Data message to reset the Dataviewer
- *  
+ * Data message to reset (clear all traces) the Dataviewer
+ * 
  * @author jasrodis
  */
 public class ResetData {
 
 	protected final String type = "RESET";
-	
+
 	private final transient Gson gson = new Gson();
 
+	/**
+	 * Serialize the ResetData object
+	 * 
+	 * @return serialized resetData Object
+	 */
 	public String serialize() {
 		return gson.toJson(this);
 	}
